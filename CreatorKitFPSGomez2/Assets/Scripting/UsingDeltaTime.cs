5a953a95ebe010adf758c2503c08420c822d9c68
stanley091610 <754083@student.dusd.net>
2025-03-24 09:50:09 -0700

    add A new scripting and light too

---
+++ CreatorKitFPSGomez2/Assets/Scripting/UsingDeltaTime.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+public class UsingDeltaTime : MonoBehaviour
+{
+    public float speed = 8f;
+    public float countdown = 3.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0.0f)
+            GetComponent<Light>().enabled = false;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
